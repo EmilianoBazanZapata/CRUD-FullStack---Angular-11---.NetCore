067bd188ebb1a8a387b8ea98b0974820a2edb336
Emiliano Bazan Zapata <maximolicanelo@gmail.com>
2021-03-28 18:47:32 -0300

    Activando Cors

--- WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/Startup.cs
@@ -26,6 +26,14 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Activando CORS 1.A
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin",
+                    options => options.AllowAnyOrigin().AllowAnyMethod());
+            });
+
+            
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -37,6 +45,9 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Activando CORS 1.B
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
