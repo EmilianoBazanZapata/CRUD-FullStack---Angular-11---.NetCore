3db8f7a395cde4361e0e3813f9dfa581c0c47d79
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-29 19:10:05 -0300

    Agregando los Componentes para trabajar con : Employee

--- Ui/FrontEnd/src/app/app.module.ts
+++ Ui/FrontEnd/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { DepartamentComponent } from './Components/departament/departament.component';
 import { ShowDepComponent } from './Components/Departament/show-dep/show-dep.component';
 import { AddEditComponent } from './Components/Departament/add-edit/add-edit.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+import { ShowEmpComponent } from './Components/Employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './Components/Employee/add-edit-emp/add-edit-emp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartamentComponent,
     ShowDepComponent,
-    AddEditComponent
+    AddEditComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
