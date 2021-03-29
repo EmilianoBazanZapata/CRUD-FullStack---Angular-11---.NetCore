429d67a0084c9a7db08430c8dfa32660d897305f
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-28 22:23:13 -0300

    Agregando Metodo Put al Controlador de Departamento

--- WebApi/WebApi/Controllers/DepartamentController.cs
+++ WebApi/WebApi/Controllers/DepartamentController.cs
@@ -85,5 +85,39 @@ namespace WebApi.Controllers
             //cargo mi elemento json con el contenido de mi tb
             return new JsonResult("Added Succesfully");
         }
+        [HttpPut]
+        public JsonResult Put(Departament dep)
+        {
+            //genero un string con la consulta hacia la BD
+            string Consulta = @"UPDATE [dbo].[DEPARTAMENT]
+                                   SET [DEPARTAMENT_NAME] = @DEPARTAMENTO
+                                 WHERE [DEPARTAMENT_ID] = @ID";
+            //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+            DataTable tb = new DataTable();
+            //creo una variable reader para capturar los datos
+            SqlDataReader MyReader;
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+
+            using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+            {
+
+                sqlcon.Open();
+                using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                {
+                    myCommand.Parameters.AddWithValue("@DEPARTAMENTO", dep.Departament_Name);
+                    myCommand.Parameters.AddWithValue("@ID", dep.Deparament_Id);
+                    MyReader = myCommand.ExecuteReader();
+                    //la tabla la cargo con los datos obtenidos de mi sentencia
+                    tb.Load(MyReader);
+                    //cierro las conexiones
+                    MyReader.Close();
+                    sqlcon.Close();
+                }
+            }
+            //cargo mi elemento json con el contenido de mi tb
+            return new JsonResult("Updated Succesfully");
+        }
     }
 }
