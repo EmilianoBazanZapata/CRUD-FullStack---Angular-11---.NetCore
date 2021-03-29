10a46d2824a34c3d093ba242056bcb00f1919096
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-28 22:37:53 -0300

    Agregando Metodo Get al ontrolador Employee

--- WebApi/WebApi/Controllers/EmployeeController.cs
+++ WebApi/WebApi/Controllers/EmployeeController.cs
@@ -4,6 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//agregamos el using 1.G
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
@@ -11,5 +16,43 @@ namespace WebApi.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        //metodo para seleccionar los departamentos
+        public JsonResult Get()
+        {
+            //genero un string con la consulta hacia la BD
+            string Consulta = @"SELECT EMPLOYEE_ID , EMPLOYEE_NAME  , DEPARTAMENT , DATE_OF_JOINING , PHOTO_FILE FROM EMPLOYEE";
+            //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+            DataTable tb = new DataTable();
+            //creo una variable reader para capturar los datos
+            SqlDataReader MyReader;
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+
+            using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+            {
+
+                sqlcon.Open();
+                using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                {
+                    MyReader = myCommand.ExecuteReader();
+                    //la tabla la cargo con los datos obtenidos de mi select 
+                    tb.Load(MyReader);
+                    //cierro las conexiones
+                    MyReader.Close();
+                    sqlcon.Close();
+                }
+            }
+            //cargo mi elemento json con el contenido de mi tb
+            return new JsonResult(tb);
+        }
     }
 }
