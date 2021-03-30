6d088f230fe7aff003a079d58f8627dd53da2392
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-30 00:42:29 -0300

    Configurando El RouterLink

--- Ui/FrontEnd/src/app/app-routing.module.ts
+++ Ui/FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent} from './Components/employee/employee.component';
+import { DepartamentComponent } from './Components/departament/departament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Employee', component:EmployeeComponent},
+  {path:'Departament',component:DepartamentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
