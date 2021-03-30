49fb3c2efa10b968233513937bcdca35914f8cb5
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-29 21:59:52 -0300

    Metodo Get para Obtener los Departamentos

--- Ui/FrontEnd/src/app/Services/shared.service.ts
+++ Ui/FrontEnd/src/app/Services/shared.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor() { }
+  readonly ApiUrl = "https://localhost:5001/api";
+  readonly PhotoUrl = "https://localhost:5001/Photos";
+
+  constructor(private http:HttpClient) 
+  {
+
+  }
+  //obtener el listado de departamentos
+  GetDepList():Observable<any[]>
+  {
+    return this.http.get<any>(this.ApiUrl + '/Departament');
+  }
+  
+
 }
