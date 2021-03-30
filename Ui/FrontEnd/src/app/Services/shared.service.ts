e136a8c0ef5edd99e364e9792f43d663ee8aee18
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-30 00:23:23 -0300

    Agregando metodo Delete para Eliminar un Empleado

--- Ui/FrontEnd/src/app/Services/shared.service.ts
+++ Ui/FrontEnd/src/app/Services/shared.service.ts
@@ -9,42 +9,43 @@ export class SharedService {
   readonly ApiUrl = "https://localhost:5001/api";
   readonly PhotoUrl = "https://localhost:5001/Photos";
 
-  constructor(private http:HttpClient) 
-  {
+  constructor(private http: HttpClient) {
 
   }
   //metodos para trabajar con DEPARTAMENTOS
   //obtener el listado de departamentos
-  GetDepList():Observable<any[]>
-  {
+  GetDepList(): Observable<any[]> {
     return this.http.get<any>(this.ApiUrl + '/Departament');
   }
   //metodo para agregar un departamento
-  AddDepartament(val:any)
-  {
-    return  this.http.post(this.ApiUrl +'/Departament',val);
+  AddDepartament(val: any) {
+    return this.http.post(this.ApiUrl + '/Departament', val);
   }
-   //metodo para actualizar un departamento
-   UpdateDepartament(val:any)
-   {
-     return  this.http.put(this.ApiUrl +'/Departament',val);
-   }
-    //metodo para eliminar un departamento
-  DeleteDepartament(val:any)
-  {
-    return  this.http.delete(this.ApiUrl +'/Departament/' + val);
+  //metodo para actualizar un departamento
+  UpdateDepartament(val: any) {
+    return this.http.put(this.ApiUrl + '/Departament', val);
+  }
+  //metodo para eliminar un departamento
+  DeleteDepartament(val: any) {
+    return this.http.delete(this.ApiUrl + '/Departament/' + val);
   }
 
   //metodos para trabajar con EMPLEADOS
-  
+
   //obtener el listado de empleados
-  GetEmpList():Observable<any[]>
-  {
+  GetEmpList(): Observable<any[]> {
     return this.http.get<any>(this.ApiUrl + '/Employee');
   }
-  //metodo para agregar un empleados
-  AddEmployee(val:any)
-  {
-    return  this.http.post(this.ApiUrl +'/Employee',val);
+  //metodo para agregar un empleado
+  AddEmployee(val: any) {
+    return this.http.post(this.ApiUrl + '/Employee', val);
+  }
+  //metodo para actualizar un empleado
+  UpdateEmployee(val: any) {
+    return this.http.put(this.ApiUrl + '/Employee', val);
+  }
+  //metodo para eliminar un Empleado
+  DeleteEmployee(val: any) {
+    return this.http.delete(this.ApiUrl + '/Employee/' + val);
   }
 }
--- WebApi/WebApi/Controllers/EmployeeController.cs
+++ WebApi/WebApi/Controllers/EmployeeController.cs
@@ -144,7 +144,37 @@ namespace WebApi.Controllers
             //cargo mi elemento json con el contenido de mi tb
             return new JsonResult("Updated Succesfully");
         }
+        [HttpDelete]
+        public JsonResult Delete(Employee emp)
+        {
+            //genero un string con la consulta hacia la BD
+            string Consulta = @"DELETE FROM [dbo].[EMPLOYEE]
+                                WHERE [EMPLOYEE_ID] = @ID";
+            //CREO UNA INSTANCIA NUEVA DE UN DATATABLE
+            DataTable tb = new DataTable();
+            //creo una variable reader para capturar los datos
+            SqlDataReader MyReader;
+            //TOMO LA CADENA CONEXXION QUE SE UBICA EN APPSETINGS.JSON
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
 
+            using (SqlConnection sqlcon = new SqlConnection(sqlDataSource))
+            {
+
+                sqlcon.Open();
+                using (SqlCommand myCommand = new SqlCommand(Consulta, sqlcon))
+                {
+                    myCommand.Parameters.AddWithValue("@ID", emp.EmployeeId);
+                    MyReader = myCommand.ExecuteReader();
+                    //la tabla la cargo con los datos obtenidos de mi sentencia
+                    tb.Load(MyReader);
+                    //cierro las conexiones
+                    MyReader.Close();
+                    sqlcon.Close();
+                }
+            }
+            //cargo mi elemento json con el contenido de mi tb
+            return new JsonResult("Deleted Succesfully");
+        }
 
         [Route("SaveFile")]
         [HttpPost]
