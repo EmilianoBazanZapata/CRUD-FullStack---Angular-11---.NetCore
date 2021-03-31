78d2d6440180ae864d662d493b4d38f5d4157547
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-31 11:56:31 -0300

    Pasando Funciones y Metodos del componene Mostrar y Ditar departamento al de Empleados

--- Ui/Ui/FrontEnd/src/app/Components/employee/show-emp/show-emp.component.ts
+++ Ui/Ui/FrontEnd/src/app/Components/employee/show-emp/show-emp.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { SharedService } from '../../../Services/shared.service';
 
 @Component({
   selector: 'app-show-emp',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowEmpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _SharedService : SharedService,
+              private _ModalService: BsModalService) { }
 
+  EmployeeList:any[];
+  ModalTitle: string;
+  ActivateAddEditDepComp: boolean;
+  emp: any;
+  ModalRef: BsModalRef;
   ngOnInit(): void {
+    this.RefreshEmpList();
   }
 
+  RefreshEmpList()
+  {
+    this._SharedService.GetEmpList().subscribe(data =>
+      {
+        this.EmployeeList = data;
+      });
+  }
+
+    //abrir modal para agregar 
+    OpenModal(template: TemplateRef<any>) {
+      this.ActivateAddEditDepComp = true;
+      this.emp =
+      {
+        deparament_Id: 0,
+        departament_Name: ""
+      }
+      this.ModalTitle = "Add Departament"
+      this.ModalRef = this._ModalService.show(template);
+    }
+    //modal para editar
+    EditClick(template: TemplateRef<any>, item) {
+      this.ActivateAddEditDepComp = true;
+      this.emp = item;
+      this.ModalTitle = "Edit Departament"
+      this.ModalRef = this._ModalService.show(template);
+    }
+    //Elimino un Departamento
+    DeleteDep(val)
+    {
+      if(confirm('Are you Sure ??'))
+      {
+        console.log(val);
+        this._SharedService.DeleteDepartament(val).subscribe(data =>
+          {
+            alert(data.toString());
+            this.RefreshEmpList();
+          })
+      }
+    }
+    //actualizo el listado
+    actualizar(val: boolean) {
+      if (val === true) {
+        this.RefreshEmpList();
+      }
+    }
+    //cerrar modal
+    Close() {
+      if (this.ModalRef) {
+        this.ActivateAddEditDepComp = false;
+        this.ModalRef.hide();
+      }
+    }
 }
